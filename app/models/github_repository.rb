16c77f01c3073fdadb67c22df9d9321645cd0a99
Nick Kelton <nkelton@sondermind.com>
2021-08-06 23:07:25 -0600

    Show repositories from profile page

--- app/models/github_repository.rb
+++ app/models/github_repository.rb
@@ -3,4 +3,19 @@ class GithubRepository < ApplicationRecord
 
   validates :github_id, uniqueness: true
   validates :name, presence: true
+
+  METADATA_ATTRS = %w[
+    html_url owner git_url private ssh_url homepage full_name
+  ].freeze
+
+  METADATA_ATTRS.each do |attribute|
+    define_method attribute do
+      metadata[attribute]
+    end
+  end
+
+  def monitoring_message
+    monitoring_notifications? ? 'Notifications are monitored' : 'Notifications are not monitored'
+  end
+
 end
