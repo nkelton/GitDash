c015dc279ad58ce049cbcb630e8b27f704742a87
Nick Kelton <nkelton@sondermind.com>
2021-08-21 23:11:58 -0600

    Add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
 require('jquery')
 require('../service_workers/service-worker')
 
